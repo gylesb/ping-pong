f960555b957ab34152f7b528a82474bc061631aa
Gyles Batara <gyles.batara@live.com>
2017-09-22 12:18:55 -0700

    Add alerts to non numbers and non-positive numbers

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,38 @@ var numbersList = function(numberInput) {
   if (numberInput > 0) {
     for (var i = 1; i <= numberInput; i++) {
       output.push(i);
+      output.forEach(function(numberInput) {
+        if (numberInput % 15 === 0) {
+          output.push("Ping Pong!");
+        } else if (numberInput % 5 === 0) {
+          output.push("Pong!")
+        } else if (numberInput % 3 === 0) {
+          output.push("Ping!");
+        } else {
+          output.push(i);
+        }
+      });
     }
-    output.forEach(function(numberInput) {
-      if (i % 15 === 0) {
-        output.push("Ping Pong!");
-      } else if (i % 5 === 0) {
-        output.push("Pong!");
-      } else if (i % 3 === 0) {
-        output.push("Ping!");
-      }
-    });
+    return output;
   }
-  return output;
-}
 
 
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var numberInput = parseInt($("#number").val());
-    var output = numbersList(numberInput)
+  $(document).ready(function() {
+    $("#formOne").submit(function(event) {
+      var numberInput = parseInt($("#number").val());
+      var output = numbersList(numberInput)
+      debugger;
+      $("#reset").click(function() {
+        $("ul#results").empty();
+      });
 
-    $("#reset").click(function() {
-      $("ul").empty();
+
+      output.forEach(function(numbers) {
+        $("#results").append("<li> " + numbers + "</li>" + "<br>");
+      })
+
+
+
+      event.preventDefault();
     });
-
-
-    output.forEach(function(numbers) {
-      $("#results").append("<li> " + numbers + "</li>");
-    })
-
-
-
-    event.preventDefault();
   });
-});
