a92ad814ee34d5448aebc032c7fa5e29379fb10a
Gyles Batara <gyles.batara@live.com>
2017-09-22 10:26:18 -0700

    add first wave of js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
-var numbersList = function(number) {
+var numbersList = function(numberInput) {
   var output = [];
 
-  alert('hi');
-
-  if (numberInput >= 0) {
+  if (numberInput > 0) {
     for (var i = 1; i <= numberInput; i++) {
-      output.push[i];
+      output.push(i);
     }
-    output.forEach(function(number) {
-      if (number % 3 === 1) {
+    output.forEach(function(numberInput) {
+      if (numberInput % 3 === 0) {
         output = "Ping";
-      } else if (number % 5 === 1) {
+      } else if (numberInput % 5 === 0) {
         output = "Pong";
-      } else if (number % 15 === 1) {
-        output = "Ping Pong!"
+      } else if (numberInput % 15 === 0) {
+        output = "Ping Pong!";
       }
     });
   }
+  return output;
 }
+
+
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    $("ul#results").empty();
     var numberInput = $("#number").val();
 
 
+
+    $("ul#results").append(output);
 
     event.preventDefault();
   });
