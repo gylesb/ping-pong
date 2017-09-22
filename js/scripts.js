a862034da5dd7f02c0a179d78710e4fb5be95565
Gyles Batara <gyles.batara@live.com>
2017-09-22 09:59:18 -0700

    introduce a couple variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
+var numbersList = function(number) {
+  var output = [];
 
+  alert('hi');
+
+  if (numberInput >= 0) {
+    for (var i = 1; i <= numberInput; i++) {
+      output.push[i];
+    }
+    output.forEach(function(number) {
+      if (number % 3 === 1) {
+        output = "Ping";
+      } else if (number % 5 === 1) {
+        output = "Pong";
+      } else if (number % 15 === 1) {
+        output = "Ping Pong!"
+      }
+    });
+  }
+}
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    var numberInput = $("#number").val();
 
 
 
